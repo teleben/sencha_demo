043019b15fb10c04a572c0916d8231a94b838f71
Ben Farmer <ben@telelanguage.com>
2011-12-01 14:59:15 -0800

    Switched to fieldset.

--- app/app.js
+++ app/app.js
@@ -12,30 +12,39 @@ Ext.application({
             	align: 'stretch'},
             defaults: {flex: 1},      
             items: [
-            	{
+        		{
             		xtype: 'panel',
+            		margin: '50 10 5 10',
             		html : [
             			'<h1>Telelanguage</h1>',
                         '<p>Speak any language with Telelanguage.</p>',
-                    ].join(""),
-               },
-               {
-	               	xtype: 'textfield',
-	               	name : 'user',
-	               	label: 'Email',
-               },
-               {
-               		xtype: 'passwordfield',
-               		name : 'pass',
-               		label: 'Password',
-               },
-               {
+                    ].join("")
+          	 	},
+               	{
+               		xtype: 'fieldset',
+            		title: 'Login',
+            		instructions: '',
+            		items: [
+            			{
+            				xtype: 'textfield',
+	               			name : 'user',
+	               			label: 'Email'
+               			},
+               			{
+               				xtype: 'passwordfield',
+               				name : 'pass',
+               				label: 'Password'
+               			}
+            		]
+	           	},
+               	{
 	               	xtype: 'panel',
+	               	margin: '50 10 5 10',
 	               	html : [
 	               		'<h2>Sign Up!</h2>',
-	               	].join(""),
-               }
-            ]
+	               	].join("")
+               	}
+        	]
         });
     }
 });
