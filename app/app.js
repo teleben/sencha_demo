1de66427e981d727683ad7804f3a0170557d822a
Ben Farmer <ben@telelanguage.com>
2011-12-01 14:40:17 -0800

    Added home template.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,41 @@
-new Ext.Application({
-    launch: function() {
-        new Ext.Panel({
+Ext.application({
+    name: 'Sencha Demo',
+
+    launch: function() { 
+        Ext.create("Ext.form.Panel", {
             fullscreen: true,
-            html: 'Hello World!'
+            title: 'Home',
+            iconCls: 'home',
+            cls: 'home',
+			layout: {
+            	type: 'vbox',
+            	align: 'stretch'},
+            defaults: {flex: 1},      
+            items: [
+            	{
+            		xtype: 'panel',
+            		html : [
+            			'<h1>Telelanguage</h1>',
+                        '<p>Speak any language with Telelanguage.</p>',
+                    ].join(""),
+               },
+               {
+	               	xtype: 'textfield',
+	               	name : 'user',
+	               	label: 'Email',
+               },
+               {
+               		xtype: 'passwordfield',
+               		name : 'pass',
+               		label: 'Password',
+               },
+               {
+	               	xtype: 'panel',
+	               	html : [
+	               		'<h2>Sign Up!</h2>',
+	               	].join(""),
+               }
+            ]
         });
     }
 });
